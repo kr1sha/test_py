97a9106c2af29bf2a3054caf6f9c3cae1ccedb65
Kr1sha <elustov@mail.ru>
2022-02-12 23:07:04 +0300

    add report_pdf functionality

--- report_proj/reports/urls.py
+++ report_proj/reports/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 
-from .views import create_report_view
+from .views import (
+    create_report_view,
+    ReportListView,
+    ReportDetailView,
+    render_pdf_view
+)
 
 app_name = 'reports'
 
 
 urlpatterns = [
-    path('save/', create_report_view, name='create-report')
+    path('save/', create_report_view, name='create-report'),
+    path('', ReportListView.as_view(), name='list'),
+    path('<int:pk>/', ReportDetailView.as_view(), name='detail'),
+    path('<int:pk>/pdf/', render_pdf_view, name='pdf')
 ]
--- report_proj/reports/views.py
+++ report_proj/reports/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
+from django.views.generic import ListView, DetailView
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
 
 from profiles.models import Profile
 
 from .utils import is_ajax, get_report_image
 from .models import Report
+
+
+class ReportListView(ListView):
+    model = Report
+    template_name = 'reports/report_list.html'
+    context_object_name = 'reports'
+
+
+class ReportDetailView(DetailView):
+    model = Report
+    template_name = 'reports/report_detail.html'
+    context_object_name = 'report'
 
 
 def create_report_view(request):
@@ -20,3 +37,24 @@ def create_report_view(request):
         return JsonResponse({'msg': 'send'})
 
     return JsonResponse({})
+
+
+def render_pdf_view(request, pk):
+    template_path = 'reports/pdf.html'
+    report = get_object_or_404(Report, pk=pk)
+    context = {'report': report}
+
+    response = HttpResponse(content_type='application/pdf')
+    # if download
+    # response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+    # if display
+    response['Content-Disposition'] = 'filename="report.pdf"'
+    template = get_template(template_path)
+    html = template.render(context)
+
+    pisa_status = pisa.CreatePDF(
+       html, dest=response)
+
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
